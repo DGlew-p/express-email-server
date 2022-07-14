7453e819f77f695153de28bcfca243b8c9fb7ef9
dGlew <darrenjglew@outlook.com>
2022-07-14 17:49:46 -0500

    disable get

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.use("/route", require("./api/route"));
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
